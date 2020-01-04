254ff572a442b8715c48c76d99e6c45293ef56d7
Kiss1414 <51024847+Kiss1414@users.noreply.github.com>
2020-01-04 18:11:03 +0500

    появилась новая feat

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-const {src, dest, watch} = require('gulp');
+const {src,dest,watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass = require("gulp-sass");
+const sass = require('gulp-sass');
 
 
 // Static server
  function bs() {
- serveSass();
- browserSync.init({
-     server: {
-         baseDir: "./"
-     }
- });
- watch("./*.html").on('change', browserSync.reload);
- watch("./sass/**/*,sass", serveSass);
- watch("./js/*.js").on('change', browserSync.reload);
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*,sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
 };
 
- function serveSass() {
+function serveSass() {
     return src("./sass/*.sass")
         .pipe(sass())
         .pipe(dest("./css"))
